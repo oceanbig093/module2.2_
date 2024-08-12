b15002f5de490a86d92f77a5a17009b48b5d2fd1
oceanbig093 <innervision41@mail.ru>
2024-08-12 11:50:10 +0300

    Add files via upload

---
+++ modle_2_2.py
@@ -0,0 +1,18 @@
+first = 6
+second = 26
+third = 6
+if first == second and second == third and first == third:
+    print('3')
+if first == second or second == third or first == third:
+    print('2')
+if first != second and second != third and first != third:
+    print('0')
+first = 15
+second = 26
+third = 6
+if first == second and second == third and first == third:
+    print('3')
+if first == second or second == third or first == third:
+    print('2')
+if not first == second or second == third or first == third:
+    print('0')
